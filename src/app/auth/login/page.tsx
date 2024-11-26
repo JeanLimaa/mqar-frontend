2829f931a6473ef510cff29735864add37d8b7bb
Jean Lima <93737943+JeanLimaa@users.noreply.github.com>
2024-11-25 21:16:59 -0300

    feat: ajustar estilos de layout nas páginas de login e registro para melhor responsividade

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/templates/auth/login/LoginForm";
 
 export default function LoginPage() {
     return(
-        <main className="min-h-screen bg-white flex flex-col p-14 flex-1 items-center justify-center">
+        <main className="min-h-screen bg-white flex flex-col p-14 flex-1 items-center justify-center max-md:p-8 max-sm:px-[0.1rem]">
             <LoginForm />
         </main>
     )
