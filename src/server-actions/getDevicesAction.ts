682baa5a8e9c37a34b6fcd3be56cbc70569bede8
Jean Lima <93737943+JeanLimaa@users.noreply.github.com>
2024-11-28 09:47:41 -0300

    feat: adicionar log de carregamento de sensores na ação getDevices; melhorar a rastreabilidade de erros

--- src/server-actions/getDevicesAction.ts
+++ src/server-actions/getDevicesAction.ts
@@ -1,4 +1,3 @@
-"use server";
 import { Sensor } from "@/interfaces/sensor.interface";
 import api from "@/services/protectedServerApiService";
 import { cache } from "react";
@@ -7,6 +6,7 @@ export const getDevices = async () => {
     try {
         const response = await api.get('/devices');
         const data: Sensor[] = response.data;
+        console.log('Sensores carregados:', data);
         return data;
     } catch (error) {
         console.error('Erro ao carregar sensores:', error);
